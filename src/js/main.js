be5074ac21335eb2aeb090a10c533bfb4e0b5b27
TalkativeDiv <abhipieznomnomnom@gmail.com>
2021-12-11 17:59:46 -0500

    🚀 first commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/* Mobile Menu */
+const menuOpen = document.querySelector(".menu");
+const menuClose = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
